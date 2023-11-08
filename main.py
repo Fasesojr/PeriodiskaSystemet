000c5699bef1bafdad03f02445ae1bb7350f9485
Fasesojr <62062384+Fasesojr@users.noreply.github.com>
2023-11-08 17:37:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 import tkinter as tk
 
-class PeriodicTableGUI(tk.Tk):
-
-
-class MenuGUI(tk.Tk):
-    """GUI class"""
-    def __init__():
+class MainGUI(tk.Tk):
+    """Main GUI for the application"""
+    def __init__(self):
         super().__init__()
 
-        #  Menu Lable
-        self.MenuLabel = tk.Label(text = "- - - - - - Menu - - - - - -")
-        self.MenuLabel.grid(row =0, column=0)
+        #  Title
+        self.TitleLabel = tk.Label(text = "- - - - - - Menu - - - - - -")
+        self.TitleLabel.grid(row = 0, column = 1)
 
-        #  Choice Number 1
-        self.Choice1 = tk.Button(text="Show All Atoms",
-                                 command = self.ShowAtoms)
-        self.Choice1.grid(row=1, column=1)
+        #  Option 1
+        self.Option1 = tk.Button(text = "1. Show the Periodic Table")
+        self.Option1.grid(row = 1, column=1)
 
-        #  Choice Number 2
-        self.Choice2 = tk.Button(text = "Excersice Atomic numbers",
-                                 command = self.AtomNumbers)
-        self.Choice2.grid(row=2, column =1)
+        #  Option 2 
+        self.Option2 = tk.Button(text = "2. Excersize the Atomic Symbols")
+        self.Option2.grid(row = 2, column = 1)
 
-        #  Choice Number 3
+        #  Option 3
+        self.Option3 = tk.Button(text = "3. Excersize the Atomic numbers")
+        self.Option3.grid(row = 3, column = 1)
+
+        #  Option 4
+        self.Option4 = tk.Button(text = "4. Close the program")
+        self.Option4.grid(row = 4, column = 1)
+
         
-
-
-
-
 
 def main():
     """Main program"""
+    window = MainGUI()
+    window.mainloop()
 
-    
-if name == "__main__":
+if __name__ == "__main__":
     main()
